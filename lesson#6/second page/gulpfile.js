0f25a161610aab89d97f9971f4eab22941983ae4
jurka <jurka_motsyak@i.ua>
2017-01-24 23:37:37 +0200

    template

---
+++ lesson#6/second page/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'), 
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync');
+
+gulp.task('sass', function () {
+	return gulp.src('app/sass/main.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+		baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', ['browser-sync', 'sass'],function() {
+	gulp.watch('app/sass/main.sass', ['sass']);
+	gulp.watch('app/index.html', browserSync.reload);
+});
